292411181d5d9b1bfaa8a3134a88ab473fa7a5db
Fragile-lab <fragile.lab@gmail.com>
2024-05-22 15:37:35 +0400

    send

--- script/script.js
+++ script/script.js
@@ -1,10 +1,13 @@
 function changeClass() {
   const buttonno = document.querySelector('.button-no');
   let count = 1;
-  buttonno.addEventListener("click", () => {
+  buttonno.addEventListener("mouseenter", () => {
     if (count < 4 && count > 0) {
       buttonno.classList.remove(`move${count - 1}`);
       buttonno.classList.add(`move${count}`);
+    } else {
+      buttonno.classList.remove(`move${count - 1}`);
+      count = 0;
     }
     count +=1;
   });
