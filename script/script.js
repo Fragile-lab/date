311b0c68c71975fcbcfcb02a4c1fceebc239397a
Fragile-lab <162269492+Fragile-lab@users.noreply.github.com>
2024-06-06 14:01:39 +0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 function changeClass() {
   const buttonno = document.querySelector('.button-no');
   let count = 1;
-  buttonno.addEventListener("mouseenter", () => {
+  buttonno.addEventListener("click", () => {
     if (count < 4 && count > 0) {
       buttonno.classList.remove(`move${count - 1}`);
       buttonno.classList.add(`move${count}`);
